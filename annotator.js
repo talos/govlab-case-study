9263b7fdca84fb1c5d07d65dda02d4c3f5ab898e
John Krauss <irving.krauss@gmail.com>
2014-08-24 13:17:43 -0400

    in-memory annotations work

--- annotator.js
+++ annotator.js
@@ -2,27 +2,62 @@
 /*jshint browser:true*/
 ;(function () {
   "use strict";
-  var db = new Firebase('https://annotator.firebaseio.com/');
+  //var db = new Firebase('https://annotator.firebaseio.com/');
   var annotations = [];
   var annotatable = 'p, div, img';
 
   function Annotatable(el) {
-    this.$el = $(el);
-    this.$el.addClass('ann-annotatable');
-    this.$el.on('click', this.toggle
+    var self = this;
+    var $el = this.$el = $(el);
+    $el.addClass('ann-annotatable');
+    var commentList = this.commentList = new CommentList(this);
+    $el.append(commentList.$el);
+    $el.on('click', function () { self.commentList.toggle(); return false; });
   }
 
-  Annotatable.prototype.update = function (data) {
-    //TODO
-    window.console.log(data);
+  function CommentList(annotatable) {
+    this.annotatable = annotatable;
+    var $el = this.$el = $('<div />').addClass('ann-comment-list');
+    this.comments = [];
+    var input = this.input = new CommentInput(this);
+    $el.append(input.$el);
+  }
+
+  CommentList.prototype.toggle = function () {
+    this.$el.toggle();
   };
 
-  function CommentInput() {
-    this.$el = $('<div />').addClass('comment-input').append($('<input />'));
+  CommentList.prototype.addComment = function (text) {
+    var comment = new Comment(this, text);
+    this.comments.push(comment);
+    this.$el.append(comment.$el);
+  };
+
+  function CommentInput(commentList) {
+    var self = this;
+    this.commentList = commentList;
+    var $input = this.$input = $('<textarea />');
+    this.$el = $('<div />').addClass('ann-comment-input')
+                           .append($input);
+
+    // Prevent annotatable from closing
+    $input.on('click', function () { return false; });
+    $input.on('blur', function () { self.submit(); });
+    $input.focus();
   }
 
-  function Comment(text) {
-    this.$el = $('<div />').addClass('comment').text(text);
+  CommentInput.prototype.submit = function () {
+    var text = this.$input.val();
+    if (text !== '') {
+      this.commentList.addComment(text);
+      this.$input.val('');
+    }
+  };
+
+  function Comment(commentList, text) {
+    this.commentList = commentList;
+    this.$el = $('<div />').addClass('ann-comment')
+                           .text(text);
   }
 
   Comment.prototype.remove = function () {
